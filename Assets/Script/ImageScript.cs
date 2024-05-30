2da5401179baf579773ab5e231b69e0bef9408b2
Jevgenija <evgenijasenjko18@gmail.com>
2024-05-30 14:15:37 +0300

    parada/slepj apgerbu

--- Assets/Script/ImageScript.cs
+++ Assets/Script/ImageScript.cs
@@ -34,4 +34,25 @@ public class ImageScript : MonoBehaviour
         }
     }
 
+    public void showChest(bool value)
+    {
+        inventoryImageHolders[0].SetActive(value);
+        inventoryImageHolders[1].SetActive(value);
+        inventoryImageHolders[2].SetActive(value);
+    }
+
+    public void showHelemt(bool value)
+    {
+        inventoryImageHolders[3].SetActive(value);
+        inventoryImageHolders[4].SetActive(value);
+        inventoryImageHolders[5].SetActive(value);
+    }
+
+    public void showWeapon(bool value)
+    {
+        inventoryImageHolders[6].SetActive(value);
+        inventoryImageHolders[7].SetActive(value);
+        inventoryImageHolders[8].SetActive(value);
+    }
+
 }
