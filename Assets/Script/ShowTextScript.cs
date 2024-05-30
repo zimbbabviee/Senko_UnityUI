60bc041da2609c1417ea18be04b28c715863c281
Jevgenija <evgenijasenjko18@gmail.com>
2024-05-30 13:14:02 +0300

    varda un gada ievade un vecuma aprekinasana

---
+++ Assets/Script/ShowTextScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowTextScript : MonoBehaviour
+{
+    string text;
+    string enteredYears = "0";
+    int age;
+    int yearNow;
+
+    public GameObject inputFieldName;
+    public GameObject inputFieldAge;
+    public GameObject displayField;
+
+
+    public void getText()
+    {
+        yearNow = System.DateTime.Now.Year;
+
+        if (inputFieldAge.GetComponent<Text>().text != "" && inputFieldAge.GetComponent<Text>().text != "-")
+        {
+            enteredYears = inputFieldAge.GetComponent<Text>().text;
+        }
+
+        age = yearNow - int.Parse(enteredYears);
+
+        text = "Cīnītājs " + inputFieldName.GetComponent<Text>().text;
+        displayField.GetComponent<Text>().text =
+                                text + " ir " + age + " gadus vecs!";
+
+
+    }
+}
