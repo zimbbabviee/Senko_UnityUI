2b9dacff42e9a7d1b07d74a002bd5f080957ccb9
Jevgenija <evgenijasenjko18@gmail.com>
2024-05-30 14:08:45 +0300

    tela apgerbs

--- Assets/Script/ImageScript.cs
+++ Assets/Script/ImageScript.cs
@@ -8,15 +8,29 @@ public class ImageScript : MonoBehaviour
     public Sprite[] spriteArray;
     public GameObject imageHolder;
 
+    public Sprite[] knightInventory;
+    public Sprite[] pirateInventory;
+    public GameObject[] inventoryImageHolders;
+
     public void changeImage(int index)
     {
         if (index == 0)
         {
             imageHolder.GetComponent<Image>().sprite = spriteArray[0];
+            for (int i = 0; i < inventoryImageHolders.Length; i++)
+            {
+                inventoryImageHolders[i].GetComponent<Image>().sprite = knightInventory[i];
+                inventoryImageHolders[i].GetComponent<Image>().SetNativeSize();
+            }
         }
         else if (index == 1)
         {
             imageHolder.GetComponent<Image>().sprite = spriteArray[1];
+            for (int i = 0; i < inventoryImageHolders.Length; i++)
+            {
+                inventoryImageHolders[i].GetComponent<Image>().sprite = pirateInventory[i];
+                inventoryImageHolders[i].GetComponent<Image>().SetNativeSize();
+            }
         }
     }
 
